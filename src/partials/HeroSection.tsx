6cb1e94a360ebc59af1337cc913fcda550656f0e
Javier Diaz Chamorro <hello@coderdiaz.me>
2020-03-03 22:23:26 -0600

    feat: added hero image for responsive

--- src/partials/HeroSection.tsx
+++ src/partials/HeroSection.tsx
@@ -33,10 +33,16 @@ const HeroSection = () => {
         </div>
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <LazyLoadImage
-          alt="Javier Diaz ― I'm a Software Engineer and teacher based on Mexico City"
-          className="h-56 w-full object-cover sm:h-72 md:h-80 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1563288181-3584b59503f9?auto=format&fit=crop&w=1200&q=10" />
+        <picture>
+          <source srcSet="https://images.unsplash.com/photo-1563288181-3584b59503f9?auto=format&fit=crop&w=320&q=80" media="(max-width: 320px)" />
+          <source srcSet="https://images.unsplash.com/photo-1563288181-3584b59503f9?auto=format&fit=crop&w=465&q=50" media="(max-width: 465px)" />
+          <source srcSet="https://images.unsplash.com/photo-1563288181-3584b59503f9?auto=format&fit=crop&w=720&q=50" media="(max-width: 720px)" />
+          <source srcSet="https://images.unsplash.com/photo-1563288181-3584b59503f9?auto=format&fit=crop&w=1200&q=80" />
+          <LazyLoadImage
+            alt="Javier Diaz ― I'm a Software Engineer and teacher based on Mexico City"
+            className="h-56 w-full object-cover sm:h-72 md:h-80 lg:w-full lg:h-full"
+            src="https://images.unsplash.com/photo-1563288181-3584b59503f9?auto=format&fit=crop&w=1200&q=80" />
+        </picture>
       </div>
     </section>
   );
