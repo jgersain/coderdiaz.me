bb791e33eb6a473c4f4fcacdb88a1b832c9567c8
Javier Diaz Chamorro <hello@coderdiaz.me>
2020-03-08 15:18:30 -0600

    fix: fixed animations styles

--- src/partials/HeroSection.tsx
+++ src/partials/HeroSection.tsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
             <p className="block text-gray-400 lg:animation-delay-80ms lg:animation-custom-cubic lg:animation-0.5s lg:animation-fill-both lg:animation-fadeInUp lg:animation-once xl:text-lg">I'm a Software Engineer and teacher based on Mexico City, Mexico. I build outstanding, high-quality websites and web applications.</p>
             <p className="hidden sm:block text-gray-400 mt-4 lg:animation-delay-95ms lg:animation-custom-cubic lg:animation-0.5s lg:animation-fill-both lg:animation-fadeInUp lg:animation-once xl:text-lg">Actually, I'm creating platforms and applications using the JAMstack architecture.</p>
             <p className="block text-gray-400 mt-4 lg:animation-delay-110ms lg:animation-custom-cubic lg:animation-0.5s lg:animation-fill-both lg:animation-fadeInUp lg:animation-once xl:text-lg">Scroll down to find out more about my work or <a className="text-white shadow-link" href="mailto: hello@coderdiaz.me">hire me</a>.</p>
-            <div className="mt-6 flex lg:animation-delay-125 lg:animation-custom-cubic lg:animation-0.5s lg:animation-fill-both lg:animation-fadeInUp lg:animation-once">
+            <div className="mt-6 flex lg:animation-delay-125ms lg:animation-custom-cubic lg:animation-0.5s lg:animation-fill-both lg:animation-fadeInUp lg:animation-once">
               <a className="text-gray-500 hover:text-white p-3 mr-2" aria-label="GitHub" href="https://github.com/coderdiaz">
                 <span className="text-2xl icon-github" />
               </a>
@@ -35,7 +35,7 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-      <div className="hero__image hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full lg:animation-clipOut lg:animation-delay-1.5s lg:animation-0.5s lg:animation-fill-both lg:animation-once">
+      <div className="hero__image hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full lg:animation-clipOut lg:animation-delay-2500ms lg:animation-0.5s lg:animation-fill-both lg:animation-once">
         <picture>
           <source
             type="image/webp"
