7399a77175ed5a9ebf7d6e3b2c25fc73d95290ce
Javier Diaz Chamorro <hello@coderdiaz.me>
2020-03-07 18:27:10 -0600

    feat: added line-height to footer paragraph

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-darken py-10">
       <div className="container px-4 md:px-8 lg:max-w-screen-lg mx-auto relative text-left sm:text-center">
         <span className="block text-gray-600 text-base">&copy; 2015 ― 2020 Javier Diaz</span>
-        <p className="text-white lg:w-150 mx-auto pt-4 text-base">This site is built with <a className="shadow-link" href="https://www.gatsbyjs.org/">Gatsby</a> & <a className="shadow-link" href="https://tailwindcss.com">TailwindCSS</a> and hosted on <a className="shadow-link" href="https://netlify.com/">Netlify</a>. <br className="hidden sm:block" />The source code is available on <a className="shadow-link" href="https://github.com/coderdiaz/coderdiaz.dev">Github</a>.</p>
+        <p className="text-white lg:w-150 mx-auto pt-4 text-base leading-loose">This site is built with <a className="shadow-link" href="https://www.gatsbyjs.org/">Gatsby</a> & <a className="shadow-link" href="https://tailwindcss.com">TailwindCSS</a> and hosted on <a className="shadow-link" href="https://netlify.com/">Netlify</a>. <br className="hidden sm:block" />The source code is available on <a className="shadow-link" href="https://github.com/coderdiaz/coderdiaz.dev">Github</a>.</p>
         <p className="text-gray-600 tracking-widest text-base pt-6">
           Made in Mexico with love <span className="icon-heart text-red-500"></span>
         </p>
