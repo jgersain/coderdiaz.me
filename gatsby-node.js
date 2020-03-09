a117a95f476b1185756a8fb3de00b95d46184db7
Javier Diaz Chamorro <hello@coderdiaz.me>
2020-03-08 23:32:25 -0600

    feat: moved logic for webpack configuration

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,21 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
-  const config = getConfig();
-
-  if (stage.startsWith('develop') && config.resolve) {
-    // Configured alias for solve temporary the react-🔥-dom warning
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'react-dom': '@hot-loader/react-dom',
-    };
-  }
-
-  // Configured module for process postcss files
-  config.module.rules.push({
-    test: /.pcss$/, use: ['style-loader', {
-      loader: 'css-loader',
-    }, 'postcss-loader'],
-  });
-}
+exports.onCreateWebpackConfig = require('./gatsby/on-create-webpack');
