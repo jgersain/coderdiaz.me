2dd1e09dcc1c38026c17cc3024a96055b60128e8
Javier Diaz Chamorro <hello@coderdiaz.me>
2020-03-08 23:27:12 -0600

    gatsby(node): added postcss-loader config

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,11 +5,20 @@
  */
 
 exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
-  const config = getConfig()
+  const config = getConfig();
+
   if (stage.startsWith('develop') && config.resolve) {
+    // Configured alias for solve temporary the react-🔥-dom warning
     config.resolve.alias = {
       ...config.resolve.alias,
-      'react-dom': '@hot-loader/react-dom'
-    }
+      'react-dom': '@hot-loader/react-dom',
+    };
   }
+
+  // Configured module for process postcss files
+  config.module.rules.push({
+    test: /.pcss$/, use: ['style-loader', {
+      loader: 'css-loader',
+    }, 'postcss-loader'],
+  });
 }
